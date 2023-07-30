5f2ea14fb4fbff298102cdd6bc52cd32d2e81c58
Sunny Changela <sunny.chnagela9@gmail.com>
2023-07-30 19:10:14 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(n)
+{
+  if (n >= 0)
+  {
+    console.log(n);
+    n--;  //n is decremented and the new value is stored back to n
+    countDown(n);  //Function is called again with the updated value
+  }
+  else
+  {
+    return;
+  }
+}
+
+countDown(10);  //Function Call
